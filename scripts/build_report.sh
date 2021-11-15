a637ef9a08cb559fdc495f49a2321914eee59388
Nick Rycar <nick@buildkite.com>
2021-11-14 18:41:53 -0800

    set timezone to fix waterfall chart

--- scripts/build_report.sh
+++ scripts/build_report.sh
@@ -7,7 +7,7 @@ echo "curl -H \"Authorization: Bearer $BUILDKITE_API_TOKEN\" \"https://api.build
 
 export BUILD_START_TIME=`curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq -r .started_at`
 echo $BUILD_START_TIME
-BUILD_START_TIME=`date -d $BUILD_START_TIME +%s`
+BUILD_START_TIME=`TZ=UTC date -d $BUILD_START_TIME +%s`
 echo $BUILD_START_TIME
 
 ./buildevents build $BUILDKITE_BUILD_ID $BUILD_START_TIME success
