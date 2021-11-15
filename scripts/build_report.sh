ae9804bfc1d07ae796f4c2a8b5bd8cd4ee611b6f
Nick Rycar <nick@buildkite.com>
2021-11-14 16:06:53 -0800

    Create a script to debug.

---
+++ scripts/build_report.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+export $BUILD_START_TIME=`curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN\" \"https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq -r .started_at`
+echo $BUILD_START_TIME
+$BUILD_START_TIME=`date -d $BUILD_START_TIME +%s`
+echo $BUILD_START_TIME
+
+./buildevents build $BUILDKITE_BUILD_ID $BUILD_START_TIME success
