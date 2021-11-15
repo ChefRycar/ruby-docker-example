2a0bc4f721c2e6bc48ae1131c861a1d2b8718de5
Nick Rycar <nick@buildkite.com>
2021-11-14 16:09:42 -0800

    un-escape quotes

--- scripts/build_report.sh
+++ scripts/build_report.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-export BUILD_START_TIME=`curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN\" \"https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq -r .started_at`
+export BUILD_START_TIME=`curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq -r .started_at`
 echo $BUILD_START_TIME
 BUILD_START_TIME=`date -d $BUILD_START_TIME +%s`
 echo $BUILD_START_TIME
