8f10baf04009e50cba6ad6252c9ef7deea0a82ff
Nick Rycar <nick@buildkite.com>
2021-11-14 16:11:07 -0800

    debugs: this time with jq

--- scripts/build_report.sh
+++ scripts/build_report.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq
+
 export BUILD_START_TIME=`curl -H "Authorization: Bearer $BUILDKITE_API_TOKEN" "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds/$BUILDKITE_BUILD_ID/" | jq -r .started_at`
 echo $BUILD_START_TIME
 BUILD_START_TIME=`date -d $BUILD_START_TIME +%s`
