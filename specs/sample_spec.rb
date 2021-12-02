fca817fe151b9812ed852ccfd5e7036c85648dc8
Nick Rycar <nick@buildkite.com>
2021-12-01 18:38:45 -0800

    test analytics setup

--- specs/sample_spec.rb
+++ specs/sample_spec.rb
@@ -1,5 +1,8 @@
 require "rspec"
 
+require "rspec/buildkite/analytics"
+RSpec::Buildkite::Analytics.configure(token: "urXKvDfr6Zrj7fXLjSpRHUou")
+
 RSpec.describe "A sample test" do
   it "runs aok" do
     expect(42).to eql(42)
